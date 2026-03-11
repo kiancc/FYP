ebdc591ae607e533d69434719d5cabc66e560e94
Kian <kiancc97@gmail.com>
2026-03-11 15:53:54 +0000

    Fix incorrect import for essentia key scale

--- core/feature_extraction/__init__.py
+++ core/feature_extraction/__init__.py
@@ -1,5 +1,5 @@
 from core.feature_extraction.bpm import extract_bpm_librosa, extract_bpm_essentia
-from core.feature_extraction.key_scale import extract_key_scale
+from core.feature_extraction.key_scale import extract_essentia_key_scale
 from core.feature_extraction.pipeline import FeaturePipeline
 
-PIPELINE = FeaturePipeline([extract_bpm_librosa, extract_bpm_essentia, extract_key_scale])
+PIPELINE = FeaturePipeline([extract_bpm_librosa, extract_bpm_essentia, extract_essentia_key_scale])
